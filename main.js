b5962ac75d415baa66c9740ee65382de3f06a58f
greggtwep16 <gregfriend1@hotmail.com>
2012-05-15 00:17:35 -0500

    first test

---
+++ main.js
@@ -0,0 +1,53 @@
+var deviceInfo = function() {
+	//function is called when device is ready
+};
+
+function roundNumber(num) {
+    var dec = 3;
+    var result = Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
+    return result;
+}
+
+var accelerationWatch = null;
+
+function updateAcceleration(a) {
+    document.getElementById('x').innerHTML = roundNumber(a.x);
+    document.getElementById('y').innerHTML = roundNumber(a.y);
+    document.getElementById('z').innerHTML = roundNumber(a.z);
+}
+
+var toggleAccel = function() {
+    if (accelerationWatch !== null) {
+        navigator.accelerometer.clearWatch(accelerationWatch);
+        updateAcceleration({
+            x : "",
+            y : "",
+            z : ""
+        });
+        accelerationWatch = null;
+    } else {
+        var options = {};
+        options.frequency = 1000;
+        accelerationWatch = navigator.accelerometer.watchAcceleration(
+                updateAcceleration, function(ex) {
+                    alert("accel fail (" + ex.name + ": " + ex.message + ")");
+                }, options);
+    }
+};
+
+
+function quit() {
+    //code below to quit application from javascript
+    navigator.app.exitApp();
+}
+
+
+function Play_Game() {
+    alert("This is where I would play the game");
+}
+
+
+
+function init() {
+    document.addEventListener("deviceready", deviceInfo, true);
+}
